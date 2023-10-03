a13bbccdc6a34fc733efa24e6d478f90b58406a9
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-10-03 15:44:49 -0400

    initil commit for travel list app

---
+++ src/App.js
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴Far Away💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?😍</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>💼You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
