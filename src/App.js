774c985f3c8bcf0b3b2fa120be66981f7a66d348
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-10-04 15:56:06 -0400

    finished separating components into own folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { Logo } from "./Logo";
-import { Stats } from "./Stats";
-import { Form } from "./Form";
-import { PackingList } from "./PackingList";
+import { Logo } from "./components/Logo";
+import { Stats } from "./components/Stats";
+import { Form } from "./components/Form";
+import { PackingList } from "./components/PackingList";
 
 export default function App() {
   const [items, setItems] = useState([]);
